948500e04d9dbf9692e2ffb56abf5163432372b9
DevMohi <morphex72@gmail.com>
2023-06-01 02:17:32 +0800

    class 2 notes added

---
+++ Placement_course/Placement/src/class2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class class2 {
+    public static void main(String[] args) {
+        // //Print Pattern
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+    
+    
+        // variables 
+        // String name = "Tony Stark";
+        // int age =40;
+        // double price = 32.25;
+        // int a = 25;
+        // int b = 10;
+        // b = 20;
+
+        // name = "iron man";
+        
+    
+        // Input in java 
+        Scanner sc = new Scanner(System.in);
+        //For full string
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        //nextInt()
+        //nextFloat() 
+    }
+}
